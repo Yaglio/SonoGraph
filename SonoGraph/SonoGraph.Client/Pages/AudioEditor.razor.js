c6bcefcaecafe8719488463c43ab35e71ad218ad
David <dp.hoebarth@gmail.com>
2025-04-17 19:30:49 +0200

    Edited AudioEditor: UI, global Header Made visualiziation of sound objects

--- SonoGraph/SonoGraph.Client/Pages/AudioEditor.razor.js
+++ SonoGraph/SonoGraph.Client/Pages/AudioEditor.razor.js
@@ -1,8 +1,5 @@
-﻿var slider = document.getElementById("myRange");
-var output = document.getElementById("currVal");
-output.textContent = slider.value; // Display the default slider value
+﻿
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function () {
-    output.textContent = this.value;
+function toggleActive(div) {
+    div.classList.toggle('active');
 }
