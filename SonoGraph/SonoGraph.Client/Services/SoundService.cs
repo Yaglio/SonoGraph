ee0763a9f2cc37c4b53d0eb51753a80ad20a7e39
Arceus336699 <anna.scharinger.44@gmail.com>
2025-05-08 14:16:14 +0200

    Alles kaputt upsi

--- SonoGraph/SonoGraph.Client/Services/SoundService.cs
+++ SonoGraph/SonoGraph.Client/Services/SoundService.cs
@@ -1,23 +1,25 @@
 ﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.JSInterop;
 using SonoGraph.Client.Models;
+using SonoGraph.Client.Pages;
+using SonoGraph.Client.Services;
 
 namespace SonoGraph.Client.Services
 {
     public class SoundService
     {
-        Audio audio {  get; set; }
-        DateTime DateTime { get; set; }
-        CancellationTokenSource CancellationTokenSource { get; set; }
-        AudioPlayerService audioPlayerService { get; set; }
+        static Audio audio {  get; set; }
+        static DateTime DateTime { get; set; }
+        static CancellationTokenSource CancellationTokenSource { get; set; }
         public SoundService( WaveFormType WaveForm) {
             audio = new Audio(WaveForm, new List<Sound> { });
             DateTime = DateTime.Now;
             CancellationTokenSource = new CancellationTokenSource();
-            //audioPlayerService = new AudioPlayerService();
         }
 
-        public void processSound(Coordinate coordinate1, Coordinate coordinate2)
+        public static async Task ProcessSound(Coordinate coordinate1)
         {
+            AudioPlayerService.Initialize();
             Sound sound = new Sound(coordinate1.Y, coordinate1.X, 100.0);
             DateTime newDateTime = DateTime.Now;
             if (audio.Sounds.Count > 0)
@@ -28,10 +30,10 @@ namespace SonoGraph.Client.Services
             }
             DateTime = newDateTime;
             audio.Sounds.Add(sound);
-            playSound(sound);
+            //playSound(sound);
         }
 
-        public void playSound(Sound sound)
+        public static void PlaySound(Sound sound)
         {
             audioPlayerService.Play(sound, audio.WaveForm, CancellationTokenSource.Token);
         }
