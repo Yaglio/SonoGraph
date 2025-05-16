aa571a4eda2d44b5a1ff7fc19aacea24e83e7b70
David <dp.hoebarth@gmail.com>
2025-05-16 17:30:53 +0200

    Finished AudioEditor

--- SonoGraph/SonoGraph.Client/Services/StorageService.cs
+++ SonoGraph/SonoGraph.Client/Services/StorageService.cs
@@ -4,9 +4,9 @@ namespace SonoGraph.Client.Services
 {
     public class StorageService
     {
-        public List<Audio> Audios { get; set; } = new ();
-        public List<Audio> SelectedAudios { get; set; } = new ();
-        public List<Sound> SelectedSounds { get; set; } = new ();
-        public Dictionary<Audio, string> audioColors { get; set; } = new ();
+        public List<Audio> Audios { get; set; } = [];
+        public List<Audio> SelectedAudios { get; set; } = [];
+        public List<Sound> SelectedSounds { get; set; } = [];
+        public Dictionary<Audio, string> audioColors { get; set; } = [];
     }
 }
